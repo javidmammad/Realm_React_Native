d516a6e206500c6d21e3dd92da182db9a07b6f2d
javidmammad <cavidmamedof61@gmail.com>
2022-04-12 21:31:49 +0400

    Realm main funcs complete

--- App.js
+++ App.js
@@ -1,112 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import Realm from 'realm';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const TaskSchema = {
+  name: "Task",
+  properties: {
+    _id: "int",
+    name: "string",
+    status: "string?",
+  },
+  primaryKey: "_id",
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+(async () => {
+  const realm = await Realm.open({
+    path: "myrealm",
+    schema: [TaskSchema],
+  });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // ### write to DB
+  // let task1, task2;
+  // realm.write(() => {
+  //   task1 = realm.create("Task", {
+  //     _id: 1,
+  //     name: "My first task",
+  //     dateCreated: Date.now(),
+  //     status: "Open"
+  //   })
+  //   task1 = realm.create("Task", {
+  //     _id: 2,
+  //     name: "My Last task",
+  //     dateCreated: Date.now(),
+  //     status: "Closed"
+  //   })
+  // })
+  // console.log(`created two tasks: ${task1.name} & ${task2.name}`);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  // ### read from DB
+  // const tasks = realm.objects("Task")
+  // console.log("All tasks", tasks)
+  // const task = realm.objectForPrimaryKey("Task", 1)
+  // console.log("one task", task.name)
+})()
+
+const App = () => {
+  return null;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
